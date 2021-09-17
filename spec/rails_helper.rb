fa9783d9a1743a72a57651d9de1c9359f425ef5c
Bowling Blake <blake@blakebowling.com>
2021-09-17 12:58:24 -0400

    179408514 devise added, authenitcaion added for application controller -i.e. site wide

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,5 @@ RSpec.configure do |config|
   ### rspec testing helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::Test::ControllerHelpers, type: :request
-
-
-
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
--- spec/requests/devises_spec.rb
+++ spec/requests/devises_spec.rb
@@ -9,13 +9,14 @@ RSpec.describe "Devises", type: :request do
         follow_redirect!
         expect(response.body).to include("Log in")
       end
-
-      it "should correctly allow logged in users to visit home page" do
-        get root_path
-        #sign_in @user
-        #expect(response.body).to include("Log in")
-
-      end
+    end
+  end
+  context "when a user is logged in , it should allow home page" do
+    it "should correctly allow logged in users to visit home page" do
+      user = User.create!(email: "test@example.com", password: "test123", password_confirmation: "test123")
+      sign_in user
+      get root_path
+      expect(response.body).to include("Home#index")
     end
   end
 end
